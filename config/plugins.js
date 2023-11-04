d9d54d61338dc8f854f101eb51e0908bec693f95
Stanoid <mujahidwadmohammad@gmail.com>
2023-11-04 07:27:28 +0300

    added date table

--- config/plugins.js
+++ config/plugins.js
@@ -3,4 +3,30 @@ module.exports = ({ env }) => ({
       enabled: true,
       
     },
+    // email: {
+    //   config: {
+    //     provider: 'nodemailer',
+    //     providerOptions: {
+    //       host: env('SMTP_HOST'),
+    //       port: env('SMTP_PORT'),
+    //       auth: {
+    //         user: env('SMTP_USERNAME'),
+    //         pass: env('SMTP_PASSWORD'),
+    //       },
+    //       pool: true,
+    //       logger: true,
+    //       debug: true,
+    //       maxConnections: 10000,
+    //       secure: false,
+    //       tls: {
+    //         rejectUnauthorized: false,
+    //       },
+    //     },
+  
+    //     settings: {
+    //       defaultFrom: env('SMTP_FROM'),
+    //       defaultReplyTo: env('SMTP_REPLY_TO'),
+    //     },
+    //   },
+    // },
   });
