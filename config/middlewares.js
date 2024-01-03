39260351aeb7d2bc0c3300c8e8e24923fdbd7c3e
Stanoid <mujahidwadmohammad@gmail.com>
2024-01-03 20:16:44 +0300

    seprate attendence json

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,16 +9,4 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['https://dhfront-92bb9a38e0ea.herokuapp.com/'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept','Body'],
-      keepHeaderOnError: true,
-    },
-  },
-
 ];
-
-
